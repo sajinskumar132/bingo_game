0c7fe28065c5f7b6eb52610cc2edcf310bb3a5e8
sajinskumar132 <sajinskumar132@gmail.com>
2024-11-11 22:31:19 +0530

    latest changes

--- src/components/box_container/BoxContainer.jsx
+++ src/components/box_container/BoxContainer.jsx
@@ -1,23 +1,26 @@
 import React, { useEffect, useState } from "react";
 import "./boxContainerStyle.css";
 import { FunctionalityHelpers } from "../../helpers/FunctionalityHelpers";
-
-function BoxContainer({ sequences, update_selected_number,is_bot,bot_generated_value }) {
+import robot_gif from '../../assets/robot.gif'
+function BoxContainer({is_start, sequences, update_selected_number,is_bot,bot_generated_value }) {
   var bingoText = "BINGO";
   return (
     <div className="main_box_container">
       
       <div className="grid_conatiner_header_text">
         {bingoText.split("").map((item,index) => (
-          <p style={{color:sequences?.selectedWinSequences[index] ?'red':'black'}}>{item}</p>
+          <p className="bingo_text" style={{color:sequences?.selectedWinSequences[index] ?'red':'black'}}>{item}</p>
         ))}
       </div>
       <div className="grid_main_container">
         <>
         {is_bot && (
         <div className="grid_bot_visibility_container">
-          <p>BOT</p>
-          <p>Generated Value : {bot_generated_value}</p>
+          <p className="grid_bot_text">BOT</p>
+          {bot_generated_value && (
+              <p className="grid_bot_generated_values">Generated Value : {bot_generated_value}</p>
+          )}
+          <img src={robot_gif} className="grid_bot_gif"/>
         </div>
       )}
        {sequences?.randomNumbers?.map((item, index) => {
@@ -46,7 +49,10 @@ function BoxContainer({ sequences, update_selected_number,is_bot,bot_generated_v
                   : "pointer",
               }}
               onClick={() => {
-                update_selected_number(!is_bot, item);
+                if(is_start){
+                  update_selected_number(!is_bot, item);
+                }
+               
               }}
             >
               <p>{item}</p>
